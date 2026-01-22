b98acc881da26432b846255cb60de983b74c5b5e
ghostreacon37-dev <ghostreacon37@gmail.com>
2026-01-22 18:04:20 +0530

    To run newwebsitebot.js

--- testbot001.js
+++ testbot001.js
@@ -1,2 +1,2 @@
-node testbot.js https://learnwithblog.xyz https://x.com/GhostReacondev/status/1981679871513575623 \
+node testbot.js https://learnwithblog.xyz https://x.com/GhostReacondev/status/2013213212175724818?s=20 \
   --forever --interval=60000 --confirm-owned
