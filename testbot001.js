c7307a698278e3359b0d4aef2695de81b2569372
ghostreacon37-dev <ghostreacon37@gmail.com>
2026-02-21 00:33:40 +0530

    To run his bot

--- testbot001.js
+++ testbot001.js
@@ -1,2 +1,2 @@
-node testbot.js https://learnwithblog.xyz https://x.com/GhostReacondev/status/2013213212175724818?s=20 \
+node testbot.js https://learnblogs.online https://x.com/GhostReacondev/status/2024921591520641247?s=20 \
   --forever --interval=60000 --confirm-owned
